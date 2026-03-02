715b9c13594715c106375eeecdb28ef50d334964
ubonggreg <dekings2@gmail.com>
2026-03-02 23:06:24 +0100

    Modify success message

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("childForm").addEventListener("submit", function(event) 
 
     let childName = document.getElementById("childName").value;
 
-    document.getElementById("successMessage").innerText =
+    document.getElementById("welcome to Safe Future Academy,[Child Name]!").innerText =
         "Registration successful for " + childName + "!";
 
     document.getElementById("childForm").reset();
