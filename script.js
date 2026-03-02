2b52f36896be2716690fda68664634b4458d7338
ubonggreg <dekings2@gmail.com>
2026-03-02 22:44:27 +0100

    Improve homepage layout and mobile responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function showMessage() {
-    alert("Child Registration Coming Soon!");
-}
+document.getElementById("childForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let childName = document.getElementById("childName").value;
+
+    document.getElementById("successMessage").innerText =
+        "Registration successful for " + childName + "!";
+
+    document.getElementById("childForm").reset();
+});
